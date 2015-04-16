8c0cf67f43a29383025e174641e7227cbef8a505
Maria Miller <mariajmiller13@gmail.com>
2015-04-16 15:29:44 -0400

    Adding playerHealth script

---
+++ Scripts/PlayerHealth.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+
+	PlayerController playerController;
+	// PlayerShooting playerShooting;
+	bool isDead = false;
+	bool damaged = false;
+
+	// Set references and initial health
+	void Awake () {
+
+		playerController = GetComponent<PlayerController>();
+
+		currentHealth = startingHealth;
+	}
+
+	public void TakeDamage (int damage) {
+
+		damaged = true;
+
+		// Subtract damage from health.
+		currentHealth -= damage;
+
+		healthSlider.value = currentHealth;
+
+		if (currentHealth <= 0 && !isDead) {
+			Die ();
+		}
+	}
+
+	public void Die () {
+
+		// Don't call this function again.
+		isDead = true;
+
+		// TODO
+		// Destroy jet object and return to game menu.
+	}
+}
