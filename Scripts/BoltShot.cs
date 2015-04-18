cab75360dd0bf1f7b8229c7ae0b89a799c5086e6
Maria Miller <mariajmiller13@gmail.com>
2015-04-17 20:43:00 -0400

    Adding Player and bolt shooting

---
+++ Scripts/BoltShot.cs
@@ -0,0 +1,21 @@
+﻿// The JET model found in the Unity Store has to be rotated
+// 270 degrees to face forward correctly. Using the jet's
+// transform.forward does not shoot in the forward direction.
+
+
+using UnityEngine;
+using System.Collections;
+
+public class BoltShot : MonoBehaviour {
+
+	private float speed = 100;
+
+	private Transform jetTransform;
+	// Use this for initialization
+	void Start () {
+	
+		jetTransform = GameObject.Find("Jet").transform;
+		rigidbody.velocity = (-jetTransform.up * speed);
+	}
+
+}
---
+++ Scripts/PlayerShoot.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	// Laser shots.
+	private GameObject LaserBolt;
+	private GameObject jet;
+	private Transform BodyGunTopRight;
+	private Transform BodyGunBottomRight;
+	private Transform BodyGunTopLeft;
+	private Transform BodyGunBottomLeft;
+	private float laserFireRate = 0.25f;
+	private float laserNextFire;
+
+	// Destroy all shots after this time.
+	private float destroyTime = 5.0f;
+
+	void Start () {
+
+		LaserBolt = Instantiate(Resources.Load("whiteLaserBolt", typeof(GameObject))) as GameObject;
+		BodyGunTopRight = GameObject.Find("Jet/Gun/ShotSpawnRT").transform;
+		BodyGunBottomRight = GameObject.Find ("Jet/Gun001/ShotSpawnRB").transform;
+		BodyGunTopLeft = GameObject.Find ("Jet/Gun003/ShotSpawnLT").transform;
+		BodyGunBottomLeft = GameObject.Find ("Jet/Gun004/ShotSpawnLB").transform;
+	}
+	
+	void Update () {
+
+
+		if (Input.GetButton("Fire1")) {
+
+			laserNextFire = Time.time + laserFireRate;
+
+			// Instantiate laser fire.
+			Instantiate(LaserBolt, BodyGunTopRight.position, BodyGunTopRight.rotation);
+			Instantiate(LaserBolt, BodyGunTopLeft.position, BodyGunTopLeft.rotation);
+			Instantiate(LaserBolt, BodyGunBottomRight.position, BodyGunBottomRight.rotation);
+			Instantiate(LaserBolt, BodyGunBottomLeft.position, BodyGunBottomLeft.rotation);
+
+			animation.Play ("GunsShootingTop");
+			animation.Play ("GunsShootingBottom");
+
+			// Instantiate missile guns on body.
+			//Instantiate(shot, BodyGunBottomRight.position, BodyGunBottomRight.rotation);
+			//Instantiate(shot, BodyGunBottomLeft.position, BodyGunBottomLeft.rotation);
+		}
+		
+	}
+
+	void shoot () {
+
+
+
+	}
+}
