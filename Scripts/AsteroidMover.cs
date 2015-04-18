880de00d2046eaa112035ad3ed8f2bb5ef947d33
Maria Miller <mariajmiller13@gmail.com>
2015-04-18 14:21:12 -0400

    Adding asteriod mover script

---
+++ Scripts/AsteroidMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidMover : MonoBehaviour {
+
+	
+	private Transform jet;
+	
+	private float speed = 70;
+	
+	// Use this for initialization
+	void Start () {
+		
+		jet = GameObject.Find ("Jet").transform;
+		// Because the Jet Model is incorrectly rotated on import,
+		// shooting in the z-axis is transform.up, not transform.forward
+		rigidbody.velocity = (jet.up * speed);
+	}
+
+}
