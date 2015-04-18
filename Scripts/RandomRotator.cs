acd46c6fe4a2563b1f4bf6a84485eb1eacdb0428
Maria Miller <mariajmiller13@gmail.com>
2015-04-18 11:49:18 -0400

    Adding random rotation to asteroids

---
+++ Scripts/RandomRotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	private float tumble = 5;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
