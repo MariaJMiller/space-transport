a1ccd0c2b9f5e1148145449bb60f41e10842b770
Maria Miller <mariajmiller13@gmail.com>
2015-04-18 14:20:23 -0400

    Explosions

--- Scripts/DestroyByContact.cs
+++ Scripts/DestroyByContact.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject asteriodExplosion;
+	public GameObject playerExplosion;
+
 	void OnTriggerEnter(Collider other) {
 
 		if (other.tag == "Boundary") {
 			return;
 		}
+		Instantiate(asteriodExplosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") {
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
