310579c551725b6c6eb749217a6bbe53ffa88e35
Maria Miller <mariajmiller13@gmail.com>
2015-04-18 14:18:39 -0400

    Rename bolt mover script

---
+++ Scripts/BoltMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltMover : MonoBehaviour {
+
+	private Transform jet;
+
+	private float speed = 150;
+	
+	// Use this for initialization
+	void Start () {
+	
+		jet = GameObject.Find ("Jet").transform;
+		// Because the Jet Model is incorrectly rotated on import,
+		// shooting in the z-axis is transform.up, not transform.forward
+		rigidbody.velocity = (-jet.up * speed);
+	}
+
+}
